543f4f4af5c9d76d47792ef781cd7cbfa969ce4b
John Ahn <smileajw1004@gmail.com>
2022-03-06 12:56:13 +0900

    final

--- src/components/Row.js
+++ src/components/Row.js
@@ -3,6 +3,17 @@ import axios from "../api/axios";
 import MovieModal from "./MovieModal";
 import "./Row.css";
 
+// import Swiper core and required modules
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
 export default function Row({ isLargeRow, title, id, fetchUrl }) {
   const [movies, setMovies] = useState([]);
   const [modalOpen, setModalOpen] = useState(false);
@@ -26,41 +37,48 @@ export default function Row({ isLargeRow, title, id, fetchUrl }) {
   return (
     <section className="row">
       <h2>{title}</h2>
-      <div className="slider">
-        <div className="slider__arrow-left">
-          <span
-            className="arrow"
-            onClick={() => {
-              document.getElementById(id).scrollLeft -= window.innerWidth - 80;
-            }}
-          >
-            {"<"}
-          </span>
-        </div>
+      <Swiper 
+        // install Swiper modules
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        loop={true} // loop 기능을 사용할 것인지
+        breakpoints={{
+          1378: {
+            slidesPerView: 6, // 한번에 보이는 슬라이드 개수
+            slidesPerGroup: 6, // 몇개씩 슬라이드 할지
+          },
+          998: {
+            slidesPerView: 5,
+            slidesPerGroup: 5,
+          },
+          625: {
+            slidesPerView: 4,
+            slidesPerGroup: 4,
+          },
+          0: {
+            slidesPerView: 3,
+            slidesPerGroup: 3,
+          },
+        }}
+        navigation  // arrow 버튼 사용 유무 
+        pagination={{ clickable: true }} // 페이지 버튼 보이게 할지 
+      >
         <div id={id} className="row__posters">
           {movies.map((movie) => (
-            <img
-              key={movie.id}
-              className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-              src={`https://image.tmdb.org/t/p/original/${
-                isLargeRow ? movie.poster_path : movie.backdrop_path
-              } `}
-              alt={movie.name}
-              onClick={() => handleClick(movie)}
-            />
+            <SwiperSlide>
+              <img
+                key={movie.id}
+                style={{ padding: "25px 0" }}
+                className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                src={`https://image.tmdb.org/t/p/original/${
+                  isLargeRow ? movie.poster_path : movie.backdrop_path
+                } `}
+                alt={movie.name}
+                onClick={() => handleClick(movie)}
+              />
+            </SwiperSlide>
           ))}
         </div>
-        <div className="slider__arrow-right">
-          <span
-            className="arrow"
-            onClick={() => {
-              document.getElementById(id).scrollLeft += window.innerWidth - 80;
-            }}
-          >
-            {">"}
-          </span>
-        </div>
-      </div>
+      </Swiper>
 
       {modalOpen && (
         <MovieModal {...movieSelected} setModalOpen={setModalOpen} />
@@ -68,3 +86,47 @@ export default function Row({ isLargeRow, title, id, fetchUrl }) {
     </section>
   );
 }
+
+// eslint-disable-next-line no-lone-blocks
+{/* <section className="row">
+<h2>{title}</h2>
+<div className="slider">
+  <div className="slider__arrow-left">
+    <span
+      className="arrow"
+      onClick={() => {
+        document.getElementById(id).scrollLeft -= window.innerWidth - 80;
+      }}
+    >
+      {"<"}
+    </span>
+  </div>
+  <div id={id} className="row__posters">
+    {movies.map((movie) => (
+      <img
+        key={movie.id}
+        className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+        src={`https://image.tmdb.org/t/p/original/${
+          isLargeRow ? movie.poster_path : movie.backdrop_path
+        } `}
+        alt={movie.name}
+        onClick={() => handleClick(movie)}
+      />
+    ))}
+  </div>
+  <div className="slider__arrow-right">
+    <span
+      className="arrow"
+      onClick={() => {
+        document.getElementById(id).scrollLeft += window.innerWidth - 80;
+      }}
+    >
+      {">"}
+    </span>
+  </div>
+</div>
+
+{modalOpen && (
+  <MovieModal {...movieSelected} setModalOpen={setModalOpen} />
+)}
+</section> */}
